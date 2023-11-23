bcffff03aba136f4107020926e5b660944fb819c
Abdul Ahad Fatik <a.ahad@applivity.com>
2023-11-23 15:19:01 +0500

    test signalR chathub

--- API/Program.cs
+++ API/Program.cs
@@ -2,15 +2,36 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace API
 {
     public class Program
     {
-        public static Task Main(string[] args)
+        public static async Task Main(string[] args)
         {
             DotNetEnv.Env.Load();
-            return CreateWebHostBuilder(args).Build().RunAsync();
+
+            // Run the web host asynchronously
+            var host = CreateWebHostBuilder(args).Build();
+            await host.StartAsync();
+
+            // Create a connection to the SignalR hub
+            var hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://13.50.221.83/chatHub") // Replace with your hub URL
+                .Build();
+            Console.WriteLine($"<--------Get URL-------->");
+
+            await hubConnection.StartAsync();
+
+            Console.WriteLine($"<--------connection established-------->");
+
+            // Close the connection when done
+            await hubConnection.DisposeAsync();
+
+            await host.WaitForShutdownAsync();
+
+            // return CreateWebHostBuilder(args).Build().RunAsync();
         }
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
