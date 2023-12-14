fdbdcae573f4e5b9f853f8ec87ba09acdfd6bf62
Abdul Ahad Fatik <a.ahad@applivity.com>
2023-12-14 16:04:40 +0500

    add api inner exception

--- API/GraphQL/Offers/MessageMutation.cs
+++ API/GraphQL/Offers/MessageMutation.cs
@@ -45,7 +45,7 @@ namespace API.GraphQL
                 return returnmessage;
             }
             catch(Exception ex) {
-                throw new ApiException($"API Exception {ex.Message}");
+                throw new ApiException($"API Exception {ex.InnerException}");
             }
         }
 
